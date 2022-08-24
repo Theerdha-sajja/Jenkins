e9ae004b768fec16a0a06876af6548e258727b34
Theerdha <sajjasrilatha151@gmail.com>
2022-08-24 21:49:30 +0530

    new commit

---
+++ src/main/java/maven_jen_integartion/demo.java
@@ -0,0 +1,11 @@
+package maven_jen_integartion;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello theerdha");
+
+	}
+
+}
---
+++ src/test/java/maven_jen_integartion/main.java
@@ -0,0 +1,14 @@
+package maven_jen_integartion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class main {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
